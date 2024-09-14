9c8ab7b028034413286bdb463c515c5d093712f0
lggg123 <machinelearner2334@gmail.com>
2024-09-14 11:07:25 -0700

    fix: fixed return statement to return proper response

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/chatbot.module.css';
 
 const API_URL = "/api/chat";
 // Replace with your own Spark Engine project ID
-const SPARKENGINE_PROJECT_ID = '362bc660-dd60-4b89-9ab7-79560cdece73';
+const SPARKENGINE_PROJECT_ID = 'e00c0fa4-a369-4268-9d17-d8542b6e57b2';
 
 interface MessageType {
   message: string;
@@ -33,7 +33,7 @@ export const Chatbot: React.FC = () => {
       }
 
       const data = await response.json();
-      return data.data.response[0]; // Adjust based on the actual response structure
+      return data.data[0].output;
     } catch (error) {
       console.error("Error fetching the API response.", error);
       return "Sorry, something went wrong.";
